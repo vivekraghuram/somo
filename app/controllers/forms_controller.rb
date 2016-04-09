2a3a9e4cc23563ee2fc40c81e206b5051db20f22
Casey <caseytaka@berkeley.edu>
2016-04-09 05:20:00 -0700

    Fixed redundant validation

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,15 +1,13 @@
 class FormsController < ApplicationController
   def create
-    if !params[:name].nil?
-      form = Form.create(create_params)
-    end
+    form = Form.new(create_params)
     if form && form.save
       questions = params[:questions]
       if !questions.nil?
         questionNum = 1
         # create all questions in the form
         questions.each do |q|
-          question = Question.create(form: form, questionType: q[:questionType], text: q[:text], name: questionNum.to_s)
+          question = Question.new(form: form, questionType: q[:questionType], text: q[:text], name: questionNum.to_s)
           if question && question.save
             options = q[:options]
             # create all options for each question
@@ -20,7 +18,7 @@ class FormsController < ApplicationController
                 nextQ = o[:nextQuestion]
                 if !nextQ.nil?
                   # create the follow-up question if question branches
-                  nextQ = Question.create(form: form, questionType: nextQ[:questionType], text: nextQ[:text],
+                  nextQ = Question.new(form: form, questionType: nextQ[:questionType], text: nextQ[:text],
                                           name: questionNum.to_s + letters[optionNum])
                   if nextQ && nextQ.save
                     optionNum = optionNum + 1
@@ -29,7 +27,7 @@ class FormsController < ApplicationController
                     return
                   end
                 end
-                newOption = Option.create(question: question, value: o[:value], nextQuestion: nextQ.id)
+                newOption = Option.new(question: question, value: o[:value], nextQuestion: nextQ.id)
                 # if option failed to create, return error
                 if !newOption || !newOption.save
                   render_json_message(:forbidden, errors: ['Option creation failed.'])
