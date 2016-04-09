629e5b9a7d7fa210cd892d01bf971f7b2a417137
Casey <caseytaka@berkeley.edu>
2016-04-09 00:33:27 -0700

    Forms create route working

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,16 +1,18 @@
 class FormsController < ApplicationController
   def create
+    puts("hello friend")
     form = Form.create(create_params)
     if form.save
       render_json_message(:ok, message: 'Form created!')
     else
       render_json_message(:forbidden, errors: ['Form creation failed.'])
     end
-  else
+  end
 
   private
 
   def create_params
+    puts("permitting stuff")
     params.permit(:name, :intro)
   end
 end
