7541067cecf7191d3c4b821d8f5ee848b436ec4f
piccoloman <joeypereira@sbcglobal.net>
2016-04-09 02:13:28 -0700

    sends text back

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,27 +4,14 @@ class TwilioController < ApplicationController
   #before_action :get_twilio_client
   
   def recieve
-    puts params
-    # Real Credits
-    account_sid = 'ACc5f882e1e4d40eb6e854830f67a20643' 
-    auth_token = '6e6cdc5b174ee4a0ae0ef8ac7854a2d6'
-   
-    # Test creds
-    #account_sid = 'AC6016613046133ebde46069a02581cc7e'
-    #auth_token = '8a45795f032cb5fd0b5f0567b58951be' 
-     
-    # set up a client to talk to the Twilio REST API 
-    @client = Twilio::REST::Client.new account_sid, auth_token 
-     
-    @client.account.messages.create({
-      from: '+14085164672',
-      to: '+19259897643',
-      body: 'recieved message'
-    })
+    if params[:SmsStatus] == "received"
+      response_body = params[:Body]
+      response_number = params[:From]
+      self.send(response_number, response_body)
+    end
   end
 
-  def send(arg)
-     
+  def send(number, body)
     # Real Credits
     account_sid = 'ACc5f882e1e4d40eb6e854830f67a20643' 
     auth_token = '6e6cdc5b174ee4a0ae0ef8ac7854a2d6'
@@ -38,8 +25,8 @@ class TwilioController < ApplicationController
      
     @client.account.messages.create({
       from: '+14085164672',
-      to: '+19259897643',
-      body: 'recieved message'
+      to: number,
+      body: body
     })
   end
 
