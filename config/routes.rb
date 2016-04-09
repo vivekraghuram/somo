3b3ed6153d27974a2302f0bca33ddf8f4c8c77aa
gracesnoh <gracesnoh@gmail.com>
2016-04-09 01:05:08 -0700

    Create View, Added Foundation and Font Awesome, CSS

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#home'
 
+  get 'create' => 'application#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
