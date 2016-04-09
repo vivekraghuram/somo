ac8db68eeea3db8316620da933b57f2fa39ef69d
Casey <caseytaka@berkeley.edu>
2016-04-09 04:36:02 -0700

    Added create validations

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -7,31 +7,45 @@ class FormsController < ApplicationController
       questions = params[:questions]
       if !questions.nil?
         questionNum = 1
+        # create all questions in the form
         questions.each do |q|
           question = Question.create(form: form, questionType: q[:questionType], text: q[:text], name: questionNum.to_s)
           if question && question.save
             options = q[:options]
+            # create all options for each question
             if !options.nil?
               letters = 'abcdefghijklmnopqrstuvwxyz'
               optionNum = 0
               options.each do |o|
                 nextQ = o[:nextQuestion]
                 if !nextQ.nil?
+                  # create the follow-up question if question branches
                   nextQ = Question.create(form: form, questionType: nextQ[:questionType], text: nextQ[:text],
                                           name: questionNum.to_s + letters[optionNum])
+                  if nextQ && nextQ.save
                     optionNum = optionNum + 1
+                  else # if follow-up question failed to create, return error
+                    render_json_message(:forbidden, errors: ['Question follow-up creation failed.'])
+                    return
+                  end
                 end
-                Option.create(question: question, value: o[:value], nextQuestion: nextQ.id)
+                newOption = Option.create(question: question, value: o[:value], nextQuestion: nextQ.id)
+                # if option failed to create, return error
+                if !newOption || !newOption.save
+                  render_json_message(:forbidden, errors: ['Option creation failed.'])
+                  return
+                end
               end
             end
-          else
+          else # if question failed to create, return error
             render_json_message(:forbidden, errors: ['Question creation failed.'])
+            return
           end
           questionNum = questionNum + 1
         end
       end
       render_json_message(:ok, message: 'Form created!')
-    else
+    else # if form failed to create, return error
       render_json_message(:forbidden, errors: ['Form creation failed.'])
     end
   end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ActiveRecord::Base
   belongs_to :question
+  validates :value, :nextQuestion, presence: true
 end
