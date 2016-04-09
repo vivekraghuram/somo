5ec6e30982648fae57f1d68ba42104a7ea15245e
piccoloman <joeypereira@sbcglobal.net>
2016-04-09 02:46:14 -0700

    added more logic with TwilioState

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,23 +1,30 @@
 require 'twilio-ruby'
 class TwilioController < ApplicationController
-
-  #before_action :get_twilio_client
+  @twilio_number = '+14085164672'
+  @survey_start = 'Welcome to Somo surveys: reply BEGIN to start, and END to stop'
 
   def start
     form = Form.find(params[:form])
-    params[:phone]
+    self.send(params[:phone], @survey_start)
+    TwilioState.create(phone: params[:phone], state: TwilioState.states[:welcome])
   end
   
+  def status
+    puts params
+  end
+
   def recieve
     if params[:SmsStatus] == "received"
       response_body = params[:Body]
       response_number = params[:From]
       self.send(response_number, response_body)
+    else
+      puts "was not recieved"
     end
   end
 
   def send(number, body)
-    # Real Credits
+    # Real Creds
     account_sid = 'ACc5f882e1e4d40eb6e854830f67a20643' 
     auth_token = '6e6cdc5b174ee4a0ae0ef8ac7854a2d6'
    
@@ -25,20 +32,11 @@ class TwilioController < ApplicationController
     #account_sid = 'AC6016613046133ebde46069a02581cc7e'
     #auth_token = '8a45795f032cb5fd0b5f0567b58951be' 
      
-    # set up a client to talk to the Twilio REST API 
-    @client = Twilio::REST::Client.new account_sid, auth_token 
-     
+    @client = Twilio::REST::Client.new account_sid, auth_token  
     @client.account.messages.create({
-      from: '+14085164672',
+      from: @twilio_number,
       to: number,
       body: body
     })
   end
-
-  private
-
-  #def get_twilio_client
-  #  @client = Twilio::REST::Client.new 'ACc5f882e1e4d40eb6e854830f67a20643', '6e6cdc5b174ee4a0ae0ef8ac7854a2d6'
-  #  debugger
-  #end
 end
