4675de6e660075c18905ee950da39a1ff50e9845
Casey <caseytaka@berkeley.edu>
2016-07-04 14:21:42 -0400

    Save json to db

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -7,6 +7,12 @@ class FormsController < ApplicationController
 
   def create
     # save json to database
+    form = Form.new(create_params)
+    if form.save
+      render_json_message(:ok, message: 'Form created!')
+    else
+      render_json_message(:forbidden, errors: form.errors.full_messages)
+    end
   end
 
   def show
@@ -15,38 +21,50 @@ class FormsController < ApplicationController
 
   def update
     # save json
+    form = Form.find(params[:id])
+    if form.update(update_params)
+      render_json_message(:ok, message: "Form saved!")
+    else
+      render_json_message(:forbidden, errors: form.errors.full_messages)
+    end
   end
 
   def submit
-    form = Form.new(create_params)
-    if form.save
-      last_qid = nil
-      cur_qid = nil
-      questions = params[:questions]
-      if !questions.nil?
-        questions.reverse.each do |q|
-          question = Question.new(form_id: form.id, questionType: q[:questionType], text: q[:text], qname: q[:qname])
-          if question.save
-            last_qid = cur_qid
-            cur_qid = question.id
-            options = q[:options]
-            if !options.nil?
-              create_options(options, last_qid)
-            else
-              option = Option.new(question: question, value: "c", nextQuestion: last_qid)
-              if !option.save
-                render_json_message(:forbidden, errors: option.errors.full_messages)
-                return
-              end
-            end
+    form = Form.find(params[:id])
+    form_json = JSON.parse(form.json)
+    last_qid = nil
+    cur_qid = nil
+    questions = form_json['questions']
+    if !questions.nil?
+      questions.reverse.each do |q|
+        question = Question.new(form_id: form.id, questionType: q['questionType'], text: q['text'], qname: q['qname'])
+        if question.save
+          last_qid = cur_qid
+          cur_qid = question.id
+          options = q['options']
+          if !options.nil?
+            create_options(options, question, last_qid)
           else
-            render_json_message(:forbidden, errors: question.errors.full_messages)
-            return
+            option = Option.new(question: question, value: "c", nextQuestion: last_qid)
+            if !option.save
+              render_json_message(:forbidden, errors: option.errors.full_messages)
+              return
+            end
           end
+        else
+          render_json_message(:forbidden, errors: question.errors.full_messages)
+          return
         end
       end
-      form.update_attributes(:firstQuestion => cur_qid)
-      render_json_message(:ok, message: 'Form created!')
+    end
+    form.update_attributes(:firstQuestion => cur_qid)
+  end
+
+  def destroy
+    form = Form.find(params[:id])
+    if form.destroy
+      render_json_message(:ok, message: 'Successfully deleted form!',
+                                to: root_path)
     else
       render_json_message(:forbidden, errors: form.errors.full_messages)
     end
@@ -59,18 +77,23 @@ class FormsController < ApplicationController
   private
 
   def create_params
-    params.permit(:name, :intro)
+    params.permit(:json)
   end
 
-  def create_options(options, last_qid)
+  def update_params
+    params.permit(:json)
+  end
+
+  def create_options(options, question, last_qid)
     options.each do |o|
       last_fid = nil
       cur_fid = nil
-      followups = o[:questions]
+      followups = o['questions']
       if !followups.nil?
-        cur_fid = createQuestions(followups,last_fid,cur_fid, last_qid)
+        cur_fid = create_questions(followups,last_fid, cur_fid, last_qid)
+      end
       #create the option to link to next question
-      option = Option.new(question: question, value: o[:value], nextQuestion: cur_fid)
+      option = Option.new(question: question, value: o['value'], nextQuestion: cur_fid)
       if !option.save
         render_json_message(:forbidden, errors: option.errors.full_messages)
         return
@@ -80,29 +103,18 @@ class FormsController < ApplicationController
 
   def create_questions(followups, last_fid, cur_fid, last_qid)
     followups.reverse.each do |f|
-      followQuestion = Question.new(form_id: form.id, questionType: f[:questionType], text: f[:text], qname: f[:qname])
-
-      if followQuestion.save
+      follow_question = Question.new(form_id: form.id, questionType: f['questionType'], text: f['text'], qname: f['qname'])
+      if follow_question.save
         last_fid = cur_fid
-        cur_fid = followQuestion.id
-        follow_options = f[:options]
+        cur_fid = follow_question.id
+        follow_options = f['options']
         if !follow_options.nil?
-          create_options(follow_options, last_qid)
+          create_options(follow_options, follow_question, last_qid)
         else
           if last_fid.nil?
-            follow_op = Option.new(question: followQuestion, value: fo[:value], nextQuestion: last_qid)
+            follow_op = Option.new(question: follow_question, value: 'a', nextQuestion: last_qid)
           else
-            follow_op = Option.new(question: followQuestion, value: fo[:value], nextQuestion: last_fid)
-          end
-          if !follow_op.save
-            render_json_message(:forbidden, errors: follow_op.errors.full_messages)
-            return
-          end
-        else
-          if last_fid.nil?
-            follow_op = Option.new(question: followQuestion, value: 'a', nextQuestion: last_qid)
-          else
-            follow_op = Option.new(question: followQuestion, value: 'b', nextQuestion: last_fid)
+            follow_op = Option.new(question: follow_question, value: 'b', nextQuestion: last_fid)
           end
           if !follow_op.save
             render_json_message(:forbidden, errors: follow_op.errors.full_messages)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704010600) do
+ActiveRecord::Schema.define(version: 20160704055240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160704010600) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "firstQuestion"
+    t.string   "json"
   end
 
   create_table "options", force: :cascade do |t|
