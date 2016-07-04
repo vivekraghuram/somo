6be735b83be04535bf1f4c2e7675b08cfa76d3ed
Casey <caseytaka@berkeley.edu>
2016-07-04 01:11:30 -0400

    Infinite chaining of conditionals

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -6,6 +6,18 @@ class FormsController < ApplicationController
   end
 
   def create
+    # save json to database
+  end
+
+  def show
+    # return json
+  end
+
+  def update
+    # save json
+  end
+
+  def submit
     form = Form.new(create_params)
     if form.save
       last_qid = nil
@@ -19,52 +31,7 @@ class FormsController < ApplicationController
             cur_qid = question.id
             options = q[:options]
             if !options.nil?
-              options.each do |o|
-                last_fid = nil
-                cur_fid = nil
-                followups = o[:questions]
-                if !followups.nil?
-                  followups.reverse.each do |f|
-                    followQuestion = Question.new(form_id: form.id, questionType: f[:questionType], text: f[:text], qname: f[:qname])
-                    if followQuestion.save
-                      last_fid = cur_fid
-                      cur_fid = followQuestion.id
-                      follow_options = f[:options]
-                      if !follow_options.nil?
-                        follow_options.each do |fo|
-                          if last_fid.nil?
-                            follow_op = Option.new(question: followQuestion, value: fo[:value], nextQuestion: last_qid)
-                          else
-                            follow_op = Option.new(question: followQuestion, value: fo[:value], nextQuestion: last_fid)
-                          end
-                          if !follow_op.save
-                            render_json_message(:forbidden, errors: follow_op.errors.full_messages)
-                            return
-                          end
-                        end
-                      else
-                        if last_fid.nil?
-                          follow_op = Option.new(question: followQuestion, value: 'a', nextQuestion: last_qid)
-                        else
-                          follow_op = Option.new(question: followQuestion, value: 'b', nextQuestion: last_fid)
-                        end
-                        if !follow_op.save
-                          render_json_message(:forbidden, errors: follow_op.errors.full_messages)
-                          return
-                        end
-                      end
-                    else
-                      render_json_message(:forbidden, errors: followQuestion.errors.full_messages)
-                      return
-                    end
-                  end
-                  option = Option.new(question: question, value: o[:value], nextQuestion: cur_fid)
-                  if !option.save
-                    render_json_message(:forbidden, errors: option.errors.full_messages)
-                    return
-                  end
-                end
-              end
+              create_options(options, last_qid)
             else
               option = Option.new(question: question, value: "c", nextQuestion: last_qid)
               if !option.save
@@ -93,5 +60,56 @@ class FormsController < ApplicationController
 
   def create_params
     params.permit(:name, :intro)
+  end
+
+  def create_options(options, last_qid)
+    options.each do |o|
+      last_fid = nil
+      cur_fid = nil
+      followups = o[:questions]
+      if !followups.nil?
+        cur_fid = createQuestions(followups,last_fid,cur_fid, last_qid)
+      #create the option to link to next question
+      option = Option.new(question: question, value: o[:value], nextQuestion: cur_fid)
+      if !option.save
+        render_json_message(:forbidden, errors: option.errors.full_messages)
+        return
+      end
+    end
+  end
+
+  def create_questions(followups, last_fid, cur_fid, last_qid)
+    followups.reverse.each do |f|
+      followQuestion = Question.new(form_id: form.id, questionType: f[:questionType], text: f[:text], qname: f[:qname])
+
+      if followQuestion.save
+        last_fid = cur_fid
+        cur_fid = followQuestion.id
+        follow_options = f[:options]
+        if !follow_options.nil?
+          create_options(follow_options, last_qid)
+        else
+          if last_fid.nil?
+            follow_op = Option.new(question: followQuestion, value: fo[:value], nextQuestion: last_qid)
+          else
+            follow_op = Option.new(question: followQuestion, value: fo[:value], nextQuestion: last_fid)
+          end
+          if !follow_op.save
+            render_json_message(:forbidden, errors: follow_op.errors.full_messages)
+            return
+          end
+        else
+          if last_fid.nil?
+            follow_op = Option.new(question: followQuestion, value: 'a', nextQuestion: last_qid)
+          else
+            follow_op = Option.new(question: followQuestion, value: 'b', nextQuestion: last_fid)
+          end
+          if !follow_op.save
+            render_json_message(:forbidden, errors: follow_op.errors.full_messages)
+            return
+          end
+        end
+      end
+    end
   end
 end
